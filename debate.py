7b62fc55e8b19a06ca8f23086376328499c58a2c
Mark Tan <marktan@outlook.co.nz>
2025-08-28 11:55:05 +1200

    Update debate.py

--- debate.py
+++ debate.py
@@ -48,13 +48,58 @@ claude = anthropic.Anthropic()
 
 google.generativeai.configure()
 
-system_message = "You are an assistant that is great at telling jokes"
-user_prompt = "Tell a light-hearted joke for an audience of Data Scientists"
+topic = "pineapple on pizza"
 
-prompts = [
-    {"role": "system", "content": system_message},
-    {"role": "user", "content": user_prompt}
-  ]
+gpt_model = "gpt-4.1-mini"
+claude_model = "claude-3-5-haiku-latest"
 
-completion = openai.chat.completions.create(model='gpt-4o-mini', messages=prompts)
-print(completion.choices[0].message.content)
+gpt_system = "You are a chatbot who does not support the idea of " + topic + ". \
+    You are to listen and understand the points the other person says, and then reply with truthful statements \
+    that support your idea. Do your best to keep a productive conversation going. If you cannot present any other \
+    compelling case, admit it and say that you agree with the other person."
+
+claude_system = "You are a chatbot who supports the idea of " + topic + ". \
+    You are to listen and understand the points the other person says, and then reply with truthful statements \
+    that support your idea. Do your best to keep a productive conversation going. If you cannot present any other \
+    compelling case, admit it and say that you agree with the other person."
+
+def call_gpt():
+    messages = [{"role": "system", "content": gpt_system}]
+    for gpt, claude in zip(gpt_messages, claude_messages):
+        messages.append({"role": "assistant", "content": gpt})
+        messages.append({"role": "user", "content": claude})
+    completion = openai.chat.completions.create(
+        model=gpt_model,
+        messages=messages
+    )
+    return completion.choices[0].message.content
+
+def call_claude():
+    messages = []
+    for gpt, claude_message in zip(gpt_messages, claude_messages):
+        messages.append({"role": "user", "content": gpt})
+        messages.append({"role": "assistant", "content": claude_message})
+    messages.append({"role": "user", "content": gpt_messages[-1]})
+    message = claude.messages.create(
+        model=claude_model,
+        system=claude_system,
+        messages=messages,
+        max_tokens=500
+    )
+    return message.content[0].text
+
+gpt_messages = ["Hi there"]
+claude_messages = ["Hi"]
+
+print(f"GPT:\n{gpt_messages[0]}\n")
+print(f"Claude:\n{claude_messages[0]}\n")
+
+for i in range(10):
+    gpt_next = call_gpt()
+    print(f"GPT:\n{gpt_next}\n")
+    gpt_messages.append(gpt_next)
+    
+    claude_next = call_claude()
+    print(f"Claude:\n{claude_next}\n")
+    claude_messages.append(claude_next)
+
